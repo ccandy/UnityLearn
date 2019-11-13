d2f37ff320db791d5984ade326c84dcc69d1bfde
ccandy <ccunandy2001@gmail.com>
2019-11-13 18:05:55 +0800

    add observer pattern

---
+++ Assets/Scripting/ObserverPattern/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+public delegate void OutOfBoundHandler();
+
+public class ProjectileController : MonoBehaviour
+{
+    #region Field Declarations
+
+    public Vector2 projectileDirection;
+    public float projectileSpeed;
+    public bool isPlayers;
+
+
+    #endregion
+
+    
+    public event OutOfBoundHandler projectileOutOfBound;
+
+    #region Movement
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveProjectile();
+    }
+
+    private void MoveProjectile()
+    {
+        transform.Translate(projectileDirection * Time.deltaTime * projectileSpeed, Space.World);
+
+        if (ScreenBounds.OutOfBounds(transform.position))
+        {
+            if (projectileOutOfBound != null)
+            {
+                projectileOutOfBound();
+            }
+
+            
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
